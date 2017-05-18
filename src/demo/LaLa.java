48f586049d1151031b17d9ad38e1e41574d9d994
xuezhixuan <zhixuan.xue@haoji-hr.com>
2017-05-18 15:08:23 +0800

    LaLa 2e(n)

---
+++ src/demo/LaLa.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package demo;
+
+import java.math.BigDecimal;
+
+/**
+* @ClassName: LaLa
+* @Description: TODO(这里用一句话描述这个类的作用)
+* @author zhixuan.xue
+* @date 2017年5月18日 下午2:57:03
+*
+*/
+public class LaLa {
+
+	public BigDecimal func(int n){
+		
+		if(n == 0){
+			return new BigDecimal(1);
+		}
+		if(n > 0){
+			return new BigDecimal(2*func(n - 1).doubleValue());
+		}
+		return new BigDecimal(0);
+	}
+	
+	public static void main(String[] args) {
+		LaLa lala = new LaLa();
+		BigDecimal result = lala.func(64);
+		System.out.println(result.doubleValue());
+	}
+}
