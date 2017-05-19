7a86708684015cdef43552cb5317c35789d75d35
xuezhixuan <zhixuan.xue@haoji-hr.com>
2017-05-19 17:33:02 +0800

    search

---
+++ src/demo/DemoIndexReader.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package demo;
+
+import org.apache.lucene.demo.SearchFiles;
+
+/**
+* @ClassName: DemoIndexReader
+* @Description: TODO(这里用一句话描述这个类的作用)
+* @author zhixuan.xue
+* @date 2017年5月19日 下午4:29:37
+*
+*/
+public class DemoIndexReader {
+
+	public static void main(String[] args) throws Exception {
+		SearchFiles.main(args);
+	}
+}
---
+++ src/demo/DemoIndexWriter.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package demo;
+
+import org.apache.lucene.demo.IndexFiles;
+
+/**
+* @ClassName: DemoIndexWriter
+* @Description: TODO(这里用一句话描述这个类的作用)
+* @author zhixuan.xue
+* @date 2017年5月19日 下午4:25:21
+*
+*/
+public class DemoIndexWriter {
+
+	
+	public static void main(String[] args) {
+		String[] arg0 = new String[]{"-docs","E:/tttt"};
+		IndexFiles.main(arg0);
+	}
+}
