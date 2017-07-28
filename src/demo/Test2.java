e431238f0b8ccf9c245e7e1f6365225bef17ac65
xuezhixuan <zhixuan.xue@haoji-hr.com>
2017-07-28 13:29:37 +0800

    cccc

---
+++ src/demo/Test2.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package demo;
+
+/**
+* @ClassName: Test2
+* @Description: TODO(这里用一句话描述这个类的作用)
+* @author zhixuan.xue
+* @date 2017年7月28日 下午1:28:12
+*
+*/
+public class Test2 {
+
+	public static void main(String[] args) throws InterruptedException {
+		while(true){
+			Thread.sleep(100000);
+		}
+	}
+}
